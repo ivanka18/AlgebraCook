a9d822527821314b9dcd2cadb293b9a90cd3c9a5
programeri <programeri@ucione.local>
2016-12-21 21:58:01 +0100

    Dodavanje gumba za brisanje

--- routes/web.php
+++ routes/web.php
@@ -42,8 +42,18 @@ Route::group(['middleware' => 'web'], function () {
 		 - form za izmjenu odabranog recepta
 		 - spremanje izmijenjenog recepta u bazu
 	*/
+	Route::get('/recipes', 'RecipesController@index');
 	
+	Route::get('/recipes/add', 'RecipesController@add');
+	Route::post('/recipes/add', 'RecipesController@save');
+	
+	Route::get('/recipes/view/{id}', 'RecipesController@view');
+	
+	Route::get('/recipes/edit/{id}', 'RecipesController@edit');
+	Route::post('/recipes/edit', 'RecipesController@update');
 	// User
     Route::get('/profil', 'UsersController@profil');
     Route::post('/profil', 'UsersController@profil');
+	
+	
 });
